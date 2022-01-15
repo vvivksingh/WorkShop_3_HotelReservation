997c3c08e9c37742dd144a87434b87707e9bb6fc
Vivek Singh <94423696+vvivksingh@users.noreply.github.com>
2022-01-15 08:42:32 +0530

    Welcome message created

--- src/main/java/com/BridgeLabz/HotelReservation/HotelReservation.java
+++ src/main/java/com/BridgeLabz/HotelReservation/HotelReservation.java
@@ -1,8 +1,7 @@
 package com.BridgeLabz.HotelReservation;
 
 public class HotelReservation {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to hotel reservation system");
     }
-
 }
